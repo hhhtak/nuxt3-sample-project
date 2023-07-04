5d23e6e1b63b09716c887ba990218b9321b41fad
hhhtak <hhhtak@users.noreply.github.com>
2023-07-04 20:09:34 +0900

    NuxtLink設定

--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <a href="/">Home2</a>
-    <a href="/about">About2</a>
+    <NuxtLink to="/">Home2</NuxtLink>
+    <NuxtLink to="/about">About2</NuxtLink>
+    <NuxtLink to="https://google.com" target="_blank">Google2</NuxtLink>
   </nav>
 </template>
