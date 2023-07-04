692de5a07ebb6c0e9bf6309d5e37d0f5744e97e3
hhhtak <hhhtak@users.noreply.github.com>
2023-07-04 20:03:44 +0900

    LazyLoading設定追加

---
+++ pages/coupon/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Coupon Page</h1>
+    <button @click="handleClick">Coupon Get</button>
+    <LazyCoupon v-if="show" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const show = ref(false);
+const handleClick = () => {
+  show.value = !show.value;
+};
+</script>
