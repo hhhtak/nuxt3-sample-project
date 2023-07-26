43e89a24f701887229bbacaea01b597615d72e35
hhhtak <hhhtak@users.noreply.github.com>
2023-07-26 19:59:48 +0900

    画像の設定

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <NuxtLayout name="default">
     <template>ヘッダー</template>
     <h1>Main Page</h1>
+    <img src="~/assets/images/peach.png" alt="もも" />
+    <img src="~/assets/images/risu.svg" alt="リス" />
+    <img src="/favicon.ico" alt="Nuxt3 Icon" />
   </NuxtLayout>
 </template>
 <script setup>
