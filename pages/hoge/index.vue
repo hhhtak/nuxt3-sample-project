d3a2f154cad444aece0d8dd75670778019ef3250
hhhtak <hhhtak@users.noreply.github.com>
2023-07-26 20:19:24 +0900

    composables ディレクトリ, plugins ディレクトリ追加

--- pages/hoge/index.vue
+++ pages/hoge/index.vue
@@ -2,6 +2,12 @@
   <div>
     <NuxtLayout name="custom">
       <h1>Index page</h1>
+      <h2>{{ $hello("World2222") }}</h2>
+      <div>Count: {{ count }}</div>
+      <div>
+        <button @click="() => inc()">increase</button>
+        <button @click="() => dec()">decrease</button>
+      </div>
     </NuxtLayout>
   </div>
 </template>
@@ -10,4 +16,6 @@
 definePageMeta({
   layout: "custom",
 });
+const { count, inc, dec } = useCounter(100);
+const { $hello } = useNuxtApp();
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
     <template>ヘッダー</template>
-    <h1>Main Page</h1>
+    <h1>{{ $hello("Main Page") }}</h1>
     <img src="~/assets/images/peach.png" alt="もも" />
     <img src="~/assets/images/risu.svg" alt="リス" />
     <img src="/favicon.ico" alt="Nuxt3 Icon" />
@@ -11,4 +11,5 @@
 definePageMeta({
   layout: false,
 });
+const { $hello } = useNuxtApp();
 </script>
