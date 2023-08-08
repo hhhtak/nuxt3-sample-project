f517ba85af2fc959353a98745828be796af72b44
hhhtak <hhhtak@users.noreply.github.com>
2023-08-08 19:52:46 +0900

    階層ページの作成とページのネスト化

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -3,5 +3,6 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
     <NuxtLink to="https://google.com" target="_blank">Google</NuxtLink>
+    <NuxtLink :to="{ name: 'users-list' }">User list</NuxtLink>
   </nav>
 </template>
